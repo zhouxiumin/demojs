36c617caa2c1fd25cbcae2b1a3ed2dfcecca5b51
zhouxiumin <zhouxiuminmeiwen@gmail.com>
2017-08-10 15:53:41 +0800

    菜单

---
+++ public/javascript/echarts/menu.js
@@ -0,0 +1,67 @@
+/**
+ * Created by zhouxiumin on 2017/7/9.
+ */
+
+var myChart = echarts.init(document.getElementById('main'));
+window.onresize = function () {
+    myChart.resize();
+};
+myChart.showLoading();
+myChart.showLoading();
+$.get('/server/simple.gexf', function (xml) {
+    myChart.hideLoading();
+
+    var graph = echarts.dataTool.gexf.parse(xml);
+    var categories = [];
+    for (var i = 0; i < 9; i++) {
+        categories[i] = {
+            name: '类目' + i
+        };
+    }
+    graph.nodes.forEach(function (node) {
+        node.itemStyle = null;
+        node.symbolSize = 10;
+        node.value = node.symbolSize;
+        node.category = node.attributes.modularity_class;
+        // Use random x, y
+        node.x = node.y = null;
+        node.draggable = true;
+    });
+    option = {
+        title: {
+            text: 'Les Miserables',
+            subtext: 'Default layout',
+            top: 'bottom',
+            left: 'right'
+        },
+        tooltip: {},
+        legend: [{
+            // selectedMode: 'single',
+            data: categories.map(function (a) {
+                return a.name;
+            })
+        }],
+        animation: false,
+        series : [
+            {
+                name: 'Les Miserables',
+                type: 'graph',
+                layout: 'force',
+                data: graph.nodes,
+                links: graph.links,
+                categories: categories,
+                roam: true,
+                label: {
+                    normal: {
+                        position: 'right'
+                    }
+                },
+                force: {
+                    repulsion: 100
+                }
+            }
+        ]
+    };
+
+    myChart.setOption(option);
+}, 'xml');
