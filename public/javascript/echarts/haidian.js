55b9469f9ad3159ce07412414c70ae377816896a
Zhou Xiumin <zhouxiuminmeiwen@gmail.com>
2017-12-14 12:47:02 +0800

    geojson

---
+++ public/javascript/echarts/haidian.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Xiumin on 2017/12/14.
+ */
+$.get('/server/haidian.json', function (haidianJson) {
+    echarts.registerMap('haidian', haidianJson);
+    var chart = echarts.init(document.getElementById('main'));
+    chart.setOption({
+        backgroundColor: '#2f6297',
+        title : {
+            text: '各地区状态统计',
+            left: 'left',
+            textStyle : {
+                color: '#fff'
+            }
+        },
+        series: [{
+            type: 'map',
+            map: 'haidian'
+        }]
+    });
+});
