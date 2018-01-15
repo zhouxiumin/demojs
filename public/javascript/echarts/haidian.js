971d3975205ee03d371adeb0ea9f9c16d9cf5579
Zhou Xiumin <zhouxiuminmeiwen@gmail.com>
2018-01-15 17:28:45 +0800

    geojson

--- public/javascript/echarts/haidian.js
+++ public/javascript/echarts/haidian.js
@@ -2,7 +2,7 @@
  * Created by Xiumin on 2017/12/14.
  */
 // $.get('/server/haidian.json', function (haidianJson) {
-$.get('/server/140500.json', function (haidianJson) {
+$.get('/server/140300.json', function (haidianJson) {
     echarts.registerMap('haidian', haidianJson);
     var chart = echarts.init(document.getElementById('main'));
     chart.setOption({
