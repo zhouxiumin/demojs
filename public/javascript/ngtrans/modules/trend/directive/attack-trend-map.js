a8144ae66dcbca482d7bd4ec1dead47fe0ac2eff
Zhou Xiumin <zhouxiuminmeiwen@gmail.com>
2018-01-22 16:53:40 +0800

    test

--- public/javascript/ngtrans/modules/trend/directive/attack-trend-map.js
+++ public/javascript/ngtrans/modules/trend/directive/attack-trend-map.js
@@ -1,119 +1,121 @@
-'use strict';
-
-/**
- * Created by huangiris on 2018/1/20.
- */
-
-
-app.directive("attackTrendMap",function(){
-    return{
-        restrict:'E',
-        scope:{mapParam:'='},
-        replace:false,
-        template: '<div style="height: 160px;width:100%" echarts="mapChart.id" config="mapChart.config"></div>',
-        controller:function ($scope,$attrs, Util, $echarts, BinhaihulianwangService) {
-            // console.log($attrs);
-            // console.log($scope.mapParam);
-            Object.assign($scope, {
-                // 地图
-                mapChart: {
-                    id: Util.uuid(),
-                    dimension: '16:2',
-                    config: {
-                        backgroundColor: 'rgba(0,0,0,0)',
-                        grid: {
-                            show:false,
-                            left: '0.5%',
-                            right: '0.5%'
+app.directive("hoursTrend",[
+    "Util",
+    "$echarts",
+    "BinhaihulianwangService",
+    function( Util, $echarts, BinhaihulianwangService){
+        return{
+            restrict:'E',
+            scope:{mapParam:'='},
+            replace:false,
+            template: '<div style="height: 160px;width:1024px" echarts="mapChart.id" config="mapChart.config"></div>',
+            controller:function ($scope) {
+                Object.assign($scope, {
+                    // 地图
+                    mapChart: {
+                        id: Util.uuid(),
+                        dimension: '16:2',
+                        config: {
+                            backgroundColor: 'rgba(0,0,0,0)',
+                            grid: {
+                                show:false,
+                                left: '0.5%',
+                                right: '0.5%'
+                            },
+                            xAxis: {},
+                            yAxis: {
+                                type: 'value',
+                                splitLine: {show: false},
+                                splitArea: {"show": false}
+                            },
+                            series: [{
+                                name: '24小时攻击次数告警趋势图',
+                                type: 'bar',
+                                data: [0]
+                            }]
                         },
-                        xAxis: {},
-                        yAxis: {
-                            type: 'value',
-                            splitLine: {show: false},
-                            splitArea: {"show": false}
-                        },
-                        series: [{
-                            name: '24小时攻击次数告警趋势图',
-                            type: 'bar',
-                            data: [0]
-                        }]
-                    },
-                    load(param) {
-                        let _this = this;
-                        // 获取数据
-                        BinhaihulianwangService.getMapAlerts(param).then(function (data) {
-                            let count = data.count;
-                            let time = data.time;
+                        load(param) {
+                            let _this = this;
+                            // 获取数据
+                            BinhaihulianwangService.getMapAlerts(param).then(function (data) {
+                                let count = data.count;
+                                let time = data.time;
 
-                            let rdata = [];
-                            for (let i = 0; i < count.length; i++) {
-                                let item = [time[i], count[i]];
-                                rdata.push(item);
-                            }
+                                let rdata = [];
+                                for (let i = 0; i < count.length; i++) {
+                                    let item = [time[i], count[i]];
+                                    rdata.push(item);
+                                }
 
-                            let times = [];
-                            for (let t of time) {
-                                times.push({
-                                    value: t, textStyle: {
-                                        color: 'red'
-                                    }
-                                });
-                            }
-                            let config = {
-                                xAxis: {
-                                    // type: 'category',
-                                    type: 'time',
-                                    splitLine: {show: false},
-                                    data: times,
-                                    axisLabel: {
-                                        textStyle: {
-                                            color: '#2996DB'
-                                        },
-                                        formatter: function (value, index) {
-                                            let date = moment(value);
-                                            return date.format('H:mm');
-
+                                let times = [];
+                                for (let t of time) {
+                                    times.push({
+                                        value: t, textStyle: {
+                                            color: 'red'
                                         }
-                                    },
-                                    min: function (value) {
-                                        return value.min - 60 * 30 * 1000 / 2;
-                                    },
-                                    max: function (value) {
-                                        return value.max + 60 * 30 * 1000 / 2;
-                                    }
-                                },
-                                series: [{
-                                    name: '24小时攻击次数告警趋势图',
-                                    type: 'bar',
-                                    itemStyle: {
-                                        normal: {
-                                            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                                                offset: 0,
-                                                color: '#FFFFFF'
-                                            }, {
-                                                offset: 0.25,
+                                    });
+                                }
+                                let config = {
+                                    xAxis: {
+                                        // type: 'category',
+                                        type: 'time',
+                                        splitLine: {show: false},
+                                        data: times,
+                                        axisLabel: {
+                                            textStyle: {
                                                 color: '#2996DB'
-                                            }, {
-                                                offset: 1,
-                                                color: '#2C5485'
-                                            }]),
-                                            shadowColor: 'rgba(0, 0, 0, 0.4)',
-                                            shadowBlur: 20
+                                            },
+                                            formatter: function (value, index) {
+                                                let date = moment(value);
+                                                return date.format('H:mm');
+
+                                            }
+                                        },
+                                        min: function (value) {
+                                            return value.min - 60 * 30 * 1000 / 2;
+                                        },
+                                        max: function (value) {
+                                            return value.max + 60 * 30 * 1000 / 2;
                                         }
                                     },
-                                    data: rdata
-                                }]
-                            };
-                            $echarts.queryEchartsInstance(_this.id).then(function (instance) {
-                                instance.resize();
-                                instance.setOption(config);
-                            });
+                                    series: [{
+                                        name: '24小时攻击次数告警趋势图',
+                                        type: 'bar',
+                                        itemStyle: {
+                                            normal: {
+                                                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                                    offset: 0,
+                                                    color: '#FFFFFF'
+                                                }, {
+                                                    offset: 0.25,
+                                                    color: '#2996DB'
+                                                }, {
+                                                    offset: 1,
+                                                    color: '#2C5485'
+                                                }]),
+                                                shadowColor: 'rgba(0, 0, 0, 0.4)',
+                                                shadowBlur: 20
+                                            }
+                                        },
+                                        data: rdata
+                                    }]
+                                };
+                                $echarts.queryEchartsInstance(_this.id).then(function (instance) {
+                                    instance.resize();
+                                    instance.setOption(config);
+                                });
 
-                        });
-                    }
-                },
-            });
-            $scope.mapChart.load($scope.mapParam);
-        }
-    };
-});
+                            });
+                        }
+                    },
+                });
+                $scope.mapChart.load($scope.mapParam);
+            },
+            link:function (scope) {
+                console.log(scope);
+
+                scope.$watch('param.time.starTime', function(newVal, oldVal) {
+                    scope.mapChart.load(scope.mapParam);
+                });
+            }
+        };
+    }]);
--- src/test.js
+++ src/test.js
@@ -2,6 +2,11 @@
  * Created by Xiumin on 2018/1/16.
  */
 
-var b = [20];
-var c = ['043'];
-console.log(b < c);
+let now = new Date();
+let t= now.getTime();
+
+console.log(typeof  now);
+console.log(typeof  t);
+
+
+console.log(t);
